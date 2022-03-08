5b67b7e1369da9563815d19f5dfcf227852bf2ee
Yash-Sapre <yss052001@gmail.com>
2022-03-08 19:57:53 +0530

    api_setup

--- API/app.py
+++ API/app.py
@@ -1,12 +1,28 @@
 from flask import Flask,request
-
+import classifier
 app = Flask(__name__)
 
-@app.route("/",methods = ['GET','POST'])
-def hello_world():
+@app.route("/create",methods = ['GET','POST'])
+def create():
     if request.method == "POST":
-        print(request.form['document'])
-        print(request.form['category'])
+        content = request.get_json(silent=True)
+        print(content)
+        print(type(content))
+        for metafield in content:
+            text = content[metafield]['documents']
+            categories = content[metafield]['categories']
+            classifier.make_model(text,categories,metafield)
 
-        return '{"name":"Yash","surname":"Sapre"}'
+        return '{"status":"ok"}'
+    return "This is GET request No Use"
+
+@app.route("/predict",methods=['GET','POST'])
+def predict():
+    if request.method == "POST":
+        content = request.get_json
+        for metafield in content:
+            text = content[metafield]['document']
+            prediction = classifier.predict_text(text,metafield)
+        return prediction
+
     return "This is GET request No Use"
---
+++ API/classifier.py
@@ -0,0 +1,34 @@
+
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+from nltk.tokenize import RegexpTokenizer
+import pickle
+
+def make_model(text,categories,file_name):
+    model = MultinomialNB()
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(text)
+    train_x = pd.DataFrame(X.toarray())
+    train_y = pd.DataFrame(categories)
+    model.fit(train_x,train_y.values.flatten())
+    obj_lst = [model,vectorizer]
+    with open(file_name,'ab') as model_file:
+        pickle.dump(obj_lst,model_file)
+
+def predict_text(text,file_name):
+    with open(file_name,'ab') as model_file:
+        obj_lst = pickle.load(model_file)
+    
+    model = obj_lst[0]
+    vectorizer = obj_lst[1]
+    test_x = vectorizer.transform([text])
+    # test_x = pd.DataFrame(X.toarray())
+    prediction = model.predict(test_x)
+    return prediction
+
+
+
+
